cc353122194c31e75d50f835648818ebdd6a2023
Vinicius Duarte <vniciusdiniz@gmail.com>
2018-04-17 20:45:34 +1000

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom(){
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop +newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log("YEP Connected to server");
 });
@@ -15,6 +30,7 @@ socket.on('newMessage', function(message){
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(locationMessage){
@@ -28,6 +44,7 @@ socket.on('newLocationMessage', function(locationMessage){
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('disconnect', function() {
